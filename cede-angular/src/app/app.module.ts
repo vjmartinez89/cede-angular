7767b68334db45bf09cf4f4be45716a840002710
Victor Martinez - DV_SVE <vmartinez@todo1.net>
2019-03-22 21:02:28 -0500

    Segunda clase

--- cede-angular/src/app/app.module.ts
+++ cede-angular/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module'; 
+import { UserModule } from './users/user.module'; 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-component/app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cede-angular/src/app/users/list/list.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'user-list',
+    templateUrl:'./list.component.html',
+    styleUrls: ['./list.component.scss']
+})
+export class ListComponent{}
